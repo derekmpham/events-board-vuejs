577a13fe71b4e75cd411041fd098112cdd1d4202
Derek Pham <derekmpham@gmail.com>
2017-05-04 15:45:51 -0700

    Add form

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 new Vue({
-// We want to target the div with an id of 'events'
+// target the div with an id of 'events'
   el: '#events'
+});
+
+new Vue({
+
+	// target div with 'events' id
+	el: '#events',
+
+	// register any values or collections holding data for the app
+	data: {},
+
+	// anything within ready function will run when app loads
+	ready: function() {},
+
+	// methods we want to use in our app are registered here
+	methods: {}
 });
